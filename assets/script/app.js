8551a03157f5adcb2ad9e8d21693607f57f7f306
marllonmendez <marlonmendesor@gmail.com>
2024-01-26 10:52:54 -0300

    create: added script

---
+++ assets/script/app.js
@@ -0,0 +1,155 @@
+function message(tagName) {
+    const className = document.querySelector(tagName);
+    return className;
+}
+
+const encryptMessage = message('.encrypt-message');
+const decryptMessage = message('.decrypt-message');
+const alertMessage = message('.warning-text');
+const outputSection = message('.output-section');
+
+const letters = [
+    ['a', '@'],
+    ['b', '*'],
+    ['c', '!'],
+    ['d', '^'],
+    ['e', '#'],
+    ['f', '('],
+    ['g', ')'],
+    ['h', '-'],
+    ['i', '$'],
+    ['j', '_'],
+    ['k', '+'],
+    ['l', '='],
+    ['m', '{'],
+    ['n', '}'],
+    ['o', '%'],
+    ['p', '['],
+    ['q', ']'],
+    ['r', ':'],
+    ['s', ';'],
+    ['t', ','],
+    ['u', '&'],
+    ['v', '<'],
+    ['w', '>'],
+    ['x', '/'],
+    ['y', '?'],
+    ['z', '.']
+];
+
+function handleEmptyField() {
+    if (encryptMessage.value === '') {
+        outputSection.innerHTML = `
+            <div class='result-container'>
+                 <img src='assets/img/girl.png' alt='Girl'>            
+                 <h1 class='result-title'>Nenhuma mensagem encontrada</h>
+                 <p class='result-paragraph'>Digite um texto que você deseja criptografar ou descriptografar.</p>
+            </div>
+        `;
+    }
+}
+
+function updateOutputSection(text) {
+    outputSection.innerHTML = `
+        <div class='result-container'>
+            <textarea class='decrypt-message' cols='20' rows='10'>${text}</textarea>
+            <button class='copy-button' onclick='buttonCopy()'>Copiar</button>
+        </div>
+    `;
+}
+
+function validateText() {
+    const isValid = encryptMessage.value === encryptMessage.value.toLowerCase() && encryptMessage.value.match(/^[a-z\s]+$/);
+
+    if (!isValid) {
+        encryptMessage.value = '';
+        decryptMessage.value = '';
+    }
+
+    return isValid;
+}
+
+function invalidEntry() {
+    alertMessage.style.color = 'red';
+    setTimeout(() => {
+        alertMessage.style.color = '#495057';
+    }, 500);
+}
+
+function encrypt(stringEncrypt) {
+    stringEncrypt = stringEncrypt.toLowerCase();
+
+    for (let index = 0; index < letters.length; index++) {
+        if (stringEncrypt.includes(letters[index][0])) {
+            console.log(`Before: ${stringEncrypt}`);
+            stringEncrypt = stringEncrypt.replaceAll(letters[index][0], letters[index][1]);
+            console.log(`After: ${stringEncrypt}`);
+        }
+    }
+
+    return stringEncrypt;
+}
+
+function performsEncryption() {
+    const textEncrypt = encrypt(encryptMessage.value);
+    decryptMessage.innerText = textEncrypt;
+    encryptMessage.value = '';
+    updateOutputSection(textEncrypt);
+}
+
+function buttonEncrypt() {
+    if (encryptMessage.value.trim() === '') {
+        handleEmptyField();
+        return;
+    } else {
+        if (validateText()) {
+            performsEncryption();
+        } else {
+            invalidEntry();
+        }
+    }
+}
+
+function decrypt(stringDecrypt) {
+    stringDecrypt = stringDecrypt.toLowerCase();
+
+    for (let index = 0; index < letters.length ; index++) {
+        if(stringDecrypt.includes(letters[index][1])) {
+            stringDecrypt = stringDecrypt.replaceAll(letters[index][1], letters[index][0])
+        }
+    }
+
+    return stringDecrypt;
+}
+
+function performsDecryption() {
+    const textDecrypt = decrypt(encryptMessage.value);
+    decryptMessage.value = textDecrypt;
+    encryptMessage.value = '';
+    updateOutputSection(textDecrypt);
+}
+
+function buttonDecrypt() {
+    if (encryptMessage.value.trim() === '') {
+        handleEmptyField();
+        return;
+    } else {
+        performsDecryption();
+    }
+}
+
+function buttonCopy() {
+    const copyValue = document.createElement('textarea');
+
+    if (/^[a-z]+$/.test(decryptMessage.value)) {
+        copyValue.value = decryptMessage.value;
+    } else {
+        copyValue.value = decryptMessage.innerText;
+    }
+
+    document.body.appendChild(copyValue);
+    copyValue.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(copyValue);
+}
